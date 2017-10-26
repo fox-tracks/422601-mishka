308599034cc3f964a91536b9f2406c5772f848b7
fox-tracks <t.andriyanova@yahoo.com>
2017-10-26 22:02:36 +0300

    build done

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,116 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var rename = require("gulp-rename");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
+var del = require("del");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
 
-gulp.task("style", function() {
+gulp.task("style", function () {
   gulp.src("less/style.less")
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("sprite", function () {
+  return gulp.src("img/for_sprite/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+
+});
+
+gulp.task("html", function () {
+  return gulp.src("*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("scripts", function() {
+  return gulp.src("js/**/*.js")
+    .pipe(concat("common.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(uglify())
+    .pipe(rename("common.min.js"))
+    .pipe(gulp.dest("build/js"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("img/content-images/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("serve", function () {
   server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: "build/"
   });
 
   gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("*.html", ["html"]);
+});
+
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+
+});
+
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "sprite",
+    "html",
+    "scripts",
+    done);
+});
+
+gulp.task("build-full", function (done) {
+  run(
+    "build",
+    "images",
+    "webp",
+    done);
 });
--- js/map.js
+++ js/map.js
@@ -1,23 +1,23 @@
 var myMap;
 var myPlacemark;
 if (window.ymaps) {
-    ymaps.ready(init);
+  ymaps.ready(init);
 }
 function init() {
-    myMap = new ymaps.Map("map", {
-        center: [59.938826, 30.322876],
-        zoom: 16,
-        controls: []
-    });
-    myPlacemark = new ymaps.Placemark([59.938848, 30.322954], {
-        hintContent: "Мы здесь!"
-    }, {
-            iconLayout: "default#image",
-            iconImageHref: "img/icon-map-pin.svg",
-            iconImageSize: [67, 100],
-            iconImageOffset: [-34, -100]
+  myMap = new ymaps.Map("map", {
+    center: [59.938826, 30.322876],
+    zoom: 16,
+    controls: []
+  });
+  myPlacemark = new ymaps.Placemark([59.938848, 30.322954], {
+    hintContent: "Мы здесь!"
+  }, {
+      iconLayout: "default#image",
+      iconImageHref: "img/icon-map-pin.svg",
+      iconImageSize: [67, 100],
+      iconImageOffset: [-34, -100]
 
-        });
-    myMap.geoObjects
-        .add(myPlacemark);
+    });
+  myMap.geoObjects
+    .add(myPlacemark);
 }
